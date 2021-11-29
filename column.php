1001dd43dee88ba24f687e1e3d13c0125a6dcfb9
constracti <constracti@gmail.com>
2021-11-29 15:47:31 +0200

    fix registration column

--- column.php
+++ column.php
@@ -3,69 +3,49 @@
 if ( !defined( 'ABSPATH' ) )
 	exit;
 
-/*
- * @param $columns array
- * @return array
- */
-add_filter( 'manage_users_columns', function( $columns ) {
+add_filter( 'manage_users_columns', function( array $columns ): array {
 	$columns['kgr-userlog-reg'] = esc_html__( 'Registration', 'kgr-userlog' );
 	$columns['kgr-userlog-act'] = esc_html__( 'Action', 'kgr-userlog' );
 	return $columns;
 } );
 
-/*
- * @param $output string
- * @param $column_name string
- * @param $user_id int
- * @return string
- */
-add_action( 'manage_users_custom_column', function( $output, $column_name, $user_id ) {
+add_action( 'manage_users_custom_column', function( string $output, string $column_name, int $user_id ): string {
 	switch ( $column_name ) {
 		case 'kgr-userlog-reg':
-			$user = get_userdata( $user_id );
-			$meta = strtotime( $user->user_registered );
-			$meta += get_option( 'gmt_offset' ) * HOUR_IN_SECONDS;
-			return sprintf( '%s %s',
-				date_i18n( get_option( 'date_format' ), $meta ),
-				date_i18n( get_option( 'time_format' ), $meta )
-			);
+			$user = get_user_by( 'ID', $user_id );
+			$dt = DateTime::createFromFormat( 'Y-m-d H:i:s', $user->user_registered );
+			$dt = $dt->getTimestamp();
+			return $output . sprintf( '%s %s',
+				wp_date( get_option( 'date_format' ), $dt ),
+				wp_date( get_option( 'time_format' ), $dt ),
+			) . "\n";
 		case 'kgr-userlog-act':
-			$meta = absint( get_user_meta( $user_id, 'kgr-userlog-act', TRUE ) );
+			$meta = intval( get_user_meta( $user_id, 'kgr-userlog-act', TRUE ) );
 			if ( $meta === 0 )
-				return esc_html__( 'never', 'kgr-userlog' );
-			return sprintf( '%s %s',
+				return $output . esc_html__( 'never', 'kgr-userlog' ) . "\n";
+			return $output .= sprintf( '%s %s',
 				esc_html( human_time_diff( $meta ), $_SERVER['REQUEST_TIME'] ),
 				esc_html__( 'ago', 'kgr-userlog' )
-			);
+			) . "\n";
 		default:
 			return $output;
 	}
 }, 10, 3 );
 
-/*
- * @param $hook string
- */
-add_action( 'admin_enqueue_scripts', function( $hook ) {
-	if ( $hook !== 'users.php' )
+add_action( 'admin_enqueue_scripts', function( string $hook_suffix ): void {
+	if ( $hook_suffix !== 'users.php' )
 		return;
-	wp_enqueue_style( 'kgr-userlog-column', KGR_USERLOG_URL . 'column.css', [], NULL );
+	wp_enqueue_style( 'kgr-userlog-column', KGR_USERLOG_URL . 'column.css', [], kgr_userlog_version() );
 } );
 
-/*
- * @param $columns array
- * @return array
- */
-add_filter( 'manage_users_sortable_columns', function( $columns ) {
+add_filter( 'manage_users_sortable_columns', function( array $columns ): array {
 	$columns['kgr-userlog-reg'] = [ 'kgr-userlog-reg', TRUE ];
 	$columns['kgr-userlog-act'] = [ 'kgr-userlog-act', TRUE ];
 	return $columns;
 } );
 
-/*
- * @param $query WP_Query
- */
-add_action( 'pre_get_users', function( $query ) {
-	if ( ! current_user_can( 'list_users' ) )
+add_action( 'pre_get_users', function( WP_User_Query $query ): void {
+	if ( !current_user_can( 'list_users' ) )
 		return;
 	$orderby = $query->get( 'orderby' );
 	switch ( $orderby ) {
--- index.php
+++ index.php
@@ -5,27 +5,40 @@
  * Plugin URI: https://github.com/constracti/wp-userlog
  * Description: Adds two custom columns to the users table. The first column contains the registration time of each user. The second column displays the time interval for which each user has been inactive.
  * Author: constracti
- * Version: 1.5
+ * Version: 1.6
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: kgr-userlog
- * Domain Path: /languages
  */
 
 if ( !defined( 'ABSPATH' ) )
 	exit;
 
+// define plugin constants
 define( 'KGR_USERLOG_DIR', plugin_dir_path( __FILE__ ) );
 define( 'KGR_USERLOG_URL', plugin_dir_url( __FILE__ ) );
 define( 'KGR_USERLOG_KEY', 'kgr-userlog' );
 
-require_once KGR_USERLOG_DIR . 'column.php';
+// require php files
+$files = glob( KGR_USERLOG_DIR . '*.php' );
+foreach ( $files as $file ) {
+        if ( $file !== __FILE__ )
+                require_once( $file );
+}
 
-add_action( 'plugins_loaded', function() {
-	load_plugin_textdomain( 'kgr-userlog', FALSE, basename( dirname( __FILE__ ) ) . '/languages' );
+// return plugin version
+function kgr_userlog_version(): string {
+        $plugin_data = get_plugin_data( __FILE__ );
+        return $plugin_data['Version'];
+}
+
+// load plugin translations
+add_action( 'init', function(): void {
+        load_plugin_textdomain( 'kgr-userlog', FALSE, basename( __DIR__ ) . '/languages' );
 } );
 
-register_activation_hook( __FILE__, function() {
+// create meta upon plugin activation
+register_activation_hook( __FILE__, function(): void {
 	$users = get_users( [
 		'meta_key' => 'kgr-userlog-act',
 		'meta_compare' => 'NOT EXISTS',
@@ -35,13 +48,15 @@ register_activation_hook( __FILE__, function() {
 		update_user_meta( $user_id, 'kgr-userlog-act', 0 );
 } );
 
-add_action( 'init', function() {
+// update meta upon user action
+add_action( 'init', function(): void {
 	$user_id = get_current_user_id();
 	if ( $user_id === 0 )
 		return;
 	update_user_meta( $user_id, 'kgr-userlog-act', $_SERVER['REQUEST_TIME'] );
 } );
 
+// create meta upon user registration
 add_action( 'user_register', function( int $user_id ) {
 	update_user_meta( $user_id, 'kgr-userlog-act', 0 );
 } );
